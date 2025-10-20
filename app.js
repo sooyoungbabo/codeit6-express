d66d14c71cb399da04b5e71b9130630e5102398e
Sooyoung Chung <chung.sooyoung@gmail.com>
2025-10-20 14:34:19 +0900

    1st: start basic express app

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const PORT = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express');
+});
+
+app.listen(PORT, (err) => {
+  //console.log(`Server_${PORT} Started - original`);
+  console.log(`Server_${PORT} Started - modified`);
+  console.log(err);
+});
