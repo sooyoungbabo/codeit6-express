63437f425a8e618c5ae63733bea42df4ef274c90
Sooyoung Chung <chung.sooyoung@gmail.com>
2025-10-21 10:01:18 +0900

    2nd commit - intro with Express

--- app.js
+++ app.js
@@ -1,14 +1,82 @@
 import express from 'express';
+import tasks from './mock.js';
 
 const PORT = 3000;
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello Express');
 });
 
+app.post('/tasks', (req, res) => {
+  const data = req.body;
+  const ids = tasks.map((task) => task.id);
+  const nextID = Math.max(...ids) + 1;
+  const now = new Date();
+  const newTask = {
+    id: nextID,
+    ...data,
+    createdAt: now,
+    updatedAt: now,
+    isComplete: false,
+  };
+  tasks.push(newTask);
+
+  res.status(201).send(newTask);
+});
+
+app.get('/tasks', (req, res) => {
+  const sort = req.query.sort;
+
+  const compareFn = sort === 'oldest' ? (a, b) => a.createdAt - b.createdAt : (a, b) => b.createdAt - a.createdAt;
+  let newTasks = tasks.sort(compareFn);
+
+  const count = Number(req.query.count);
+  if (count) {
+    newTasks = newTasks.slice(0, count);
+  }
+  res.send(newTasks);
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.find((task) => task.id === id);
+  if (task) {
+    res.send(task);
+  } else {
+    res.status(404).send({ message: 'ID not found' });
+  }
+});
+
+app.patch('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.find((task) => task.id === id);
+
+  if (task) {
+    Object.keys(req.body).forEach((key) => {
+      task[key] = req.body[key];
+    });
+    task.updatedAt = new Date();
+    res.send(task);
+  } else {
+    res.status(404).send({ message: 'ID not found' });
+  }
+});
+
+app.delete('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+
+  if (taskIndex !== -1) {
+    const [deletedTask] = tasks.splice(taskIndex, 1);
+    res.send(deletedTask);
+  } else {
+    res.status(404).send({ message: 'ID not found' });
+  }
+});
+
 app.listen(PORT, (err) => {
-  //console.log(`Server_${PORT} Started - original`);
-  console.log(`Server_${PORT} Started - modified`);
+  console.log(`Server_${PORT} Started`);
   console.log(err);
 });
